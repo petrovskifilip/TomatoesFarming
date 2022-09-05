9a17825787f76d7720d524ce7bbf390a9f914a7f
Filip Petrovski <filip.petrovski.1@students.finki.ukim.mk>
2022-09-05 12:55:35 +0200

    Second deployment fix static

--- TomatoesFarming/settings.py
+++ TomatoesFarming/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -118,6 +119,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 
 STATIC_URL = 'static/'
 
