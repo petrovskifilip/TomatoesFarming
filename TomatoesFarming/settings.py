068ff213b5cd16e814611e73539a4272e4f3ef85
Filip Petrovski <filip.petrovski.1@students.finki.ukim.mk>
2022-09-05 12:50:44 +0200

    Second deployment

--- TomatoesFarming/settings.py
+++ TomatoesFarming/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&c6&xak6)zg#ku-eh=q7zgofkoboa%0suo&2^h@em-a%l*8s+t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tomatoes-farming.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
